4362f7de5a3e1ce0a1d1d3e024c278dbb61b123b
vyogita <89368933+vyogita@users.noreply.github.com>
2021-10-09 14:39:40 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,22 +1,40 @@
-package com.qascript;
+//package com.qascript;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;  
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver; 
 
 public class FirstTest {
 
 
-    @Test
-    public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-         WebDriver driver=new ChromeDriver();  
+    public static void main(String[] args) {  
+        
+    // declaration and instantiation of objects/variables  
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");  
+    WebDriver driver=new ChromeDriver();  
       
 // Launch website  
-    driver.navigate().to("http://www.google.com/");
+    driver.navigate().to("http://www.google.com/");  
+          
+    // Click on the search text box and send value  
+    driver.findElement(By.id("lst-ib")).sendKeys("javatpoint tutorials");  
+          
+    // Click on the search button  
+    driver.findElement(By.name("btnK")).click();  
+      
+    }  
+  
+}  
+ //   @Test
+ //   public void OpenBrowser()  {
+  //      WebDriver driver;
+   //     System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        
       // ChromeOptions options = new ChromeOptions();
        // options.addArguments("headless");
        // options.addArguments("disable-gpu");
@@ -24,9 +42,9 @@ public class FirstTest {
       //  driver.get("https://www.google.com");
       //  System.out.println("Title of the page is: " + driver.getTitle());
        // Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-    }
+//    }
 
 
-}
+// }
 
 
