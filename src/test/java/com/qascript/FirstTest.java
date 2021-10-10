6ad9b0e5fa72267ae155e162e4e9cad7f2ed4866
vyogita <89368933+vyogita@users.noreply.github.com>
2021-10-10 20:03:05 +0530

    Update FirstTest.java

--- src/test/java/com/qascript/FirstTest.java
+++ src/test/java/com/qascript/FirstTest.java
@@ -1,10 +1,4 @@
-//package com.qascript;
 
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.By;  
 import org.openqa.selenium.WebDriver;  
 import org.openqa.selenium.chrome.ChromeDriver; 
@@ -30,21 +24,6 @@ public class FirstTest {
     }  
   
 }  
- //   @Test
- //   public void OpenBrowser()  {
-  //      WebDriver driver;
-   //     System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        
-      // ChromeOptions options = new ChromeOptions();
-       // options.addArguments("headless");
-       // options.addArguments("disable-gpu");
-       // driver = new ChromeDriver(options);
-      //  driver.get("https://www.google.com");
-      //  System.out.println("Title of the page is: " + driver.getTitle());
-       // Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-//    }
-
-
-// }
+ 
 
 
